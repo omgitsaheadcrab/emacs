f283400abbe754e441f535ec78c9f7e06159f768
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2020-04-02 15:34:13 +0100

    feat: allow for package installation from source

--- init.el
+++ init.el
@@ -35,6 +35,17 @@
   ;; Update installed packages at startup if there is an update pending.
   (auto-package-update-maybe))
 
+;; Allow package installs from source
+(use-package quelpa
+  :custom (quelpa-upgrade-p t "Always try to update packages")
+  :config
+  ;; Get ‘quelpa-use-package’ via ‘quelpa’
+  (quelpa
+   '(quelpa-use-package
+     :fetcher git
+     :url "https://github.com/quelpa/quelpa-use-package.git"))
+  (require 'quelpa-use-package))
+
 ;; Load remainder of config from config.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
